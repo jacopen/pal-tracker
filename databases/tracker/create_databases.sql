00f26763aeb8956be04c34ee44cfaa3d7d8c5449
Ken Ojiri <kojiri@pivotal.io>
2019-10-17 23:07:51 +0900

    done: JDBC

--- databases/tracker/create_databases.sql
+++ databases/tracker/create_databases.sql
@@ -7,3 +7,7 @@ CREATE DATABASE tracker_test;
 CREATE USER IF NOT EXISTS 'tracker'@'localhost' IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON tracker_dev.* TO 'tracker' @'localhost';
 GRANT ALL PRIVILEGES ON tracker_test.* TO 'tracker' @'localhost';
+
+CREATE USER IF NOT EXISTS 'tracker'@'%' IDENTIFIED BY '';
+GRANT ALL PRIVILEGES ON tracker_dev.* TO 'tracker' @'%';
+GRANT ALL PRIVILEGES ON tracker_test.* TO 'tracker' @'%';
